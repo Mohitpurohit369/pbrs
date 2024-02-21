c1f083089eb75e86abc83a16841e506ab0c94f9f
Mohit Purohit <mohitpurohit369@gmail.com>
2024-02-21 22:17:46 +0530

    aa

--- src/app/component/category/auth/login/login.component.ts
+++ src/app/component/category/auth/login/login.component.ts
@@ -20,16 +20,16 @@ export class LoginComponent {
   ngOnInit(): void {
     this.form = this.formBuilder.group(
       {
-        file: [null],
+        // file: [null],
         fullname: ['', Validators.required],
-        username: [
-          '',
-          [
-            Validators.required,
-            Validators.minLength(6),
-            Validators.maxLength(20)
-          ]
-        ],
+        // username: [
+        //   '',
+        //   [
+        //     Validators.required,
+        //     Validators.minLength(6),
+        //     Validators.maxLength(20)
+        //   ]
+        // ],
         mob: ['', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
         email: ['', [Validators.required, Validators.email]],
         password: [
@@ -57,8 +57,10 @@ export class LoginComponent {
     this.submitted = true;
 
     if (this.form.invalid) {
+      console.log("INvalid data ",this.form.value);
       return;
     }
+    console.log("valid data ",this.form.value);
 
     console.log(JSON.stringify(this.form.value, null, 2));
   }
@@ -76,35 +78,35 @@ export class LoginComponent {
 
 
 
-   @ViewChild('fileInput') el: ElementRef;
-   imageUrl: any = '/assets/dummy-user.jpg';
+  //  @ViewChild('fileInput') el: ElementRef;
+  //  imageUrl: any = '/assets/dummy-user.jpg';
    
-   uploadFile(event) {
-     let reader = new FileReader(); // HTML5 FileReader API
-     let file = event.target.files[0];
-     if (event.target.files && event.target.files[0]) {
-       reader.readAsDataURL(file);
-       // When file uploads set it to file formcontrol
-       reader.onload = () => {
-         this.imageUrl = reader.result;
-         this.form.patchValue({
-           file: reader.result
-         });
-         this.editFile = false;
-         this.removeUpload = true;
-       }
-       // ChangeDetectorRef since file is loading outside the zone
-       this.cd.markForCheck();        
-     }
-   }
-   // Function to remove uploaded file
-   removeUploadedFile() {
-     let newFileList = Array.from(this.el.nativeElement.files);
-     this.imageUrl = '/assets/dummy-user.jpg';
-     this.editFile = true;
-     this.removeUpload = false;
-     this.form.patchValue({
-       file: [null]
-     });
-   }
+  //  uploadFile(event) {
+  //    let reader = new FileReader(); // HTML5 FileReader API
+  //    let file = event.target.files[0];
+  //    if (event.target.files && event.target.files[0]) {
+  //      reader.readAsDataURL(file);
+  //      // When file uploads set it to file formcontrol
+  //      reader.onload = () => {
+  //        this.imageUrl = reader.result;
+  //        this.form.patchValue({
+  //          file: reader.result
+  //        });
+  //        this.editFile = false;
+  //        this.removeUpload = true;
+  //      }
+  //      // ChangeDetectorRef since file is loading outside the zone
+  //      this.cd.markForCheck();        
+  //    }
+  //  }
+  //  // Function to remove uploaded file
+  //  removeUploadedFile() {
+  //    let newFileList = Array.from(this.el.nativeElement.files);
+  //    this.imageUrl = '/assets/dummy-user.jpg';
+  //    this.editFile = true;
+  //    this.removeUpload = false;
+  //    this.form.patchValue({
+  //      file: [null]
+  //    });
+  //  }
 }
